5b9f84d7f5b346d74938194026bb0c5bedfbd5ca
João Mendes <62259008+cenamendes@users.noreply.github.com>
2024-03-11 11:57:34 +0000

    [upd] pdf-email

--- resources/views/tenant/mail/tasks/pdf-email.blade.php
+++ resources/views/tenant/mail/tasks/pdf-email.blade.php
@@ -54,7 +54,7 @@
                                             <div class="table" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
                                                    
                                                    <p style="font-family:Arial, Helvetica, sans-serif; color: #74787e;">
-                                                      <b>O seu pedido {{strtolower($task->tipoPedido->name)}} #{{$task->reference}} foi <b>CONCLUÍDA</b> {{$dataInicio}} - {{$horaInicio}}</b>
+                                                      <b>O seu pedido {{strtolower($task->tipoPedido->name)}} #{{$task->reference}} foi <b>CONCLUÍDA</b> {{ date('Y-m-d') }} - {{ date('H:i:s') }}</b>
                                                    </p>
                                                    {{-- @if($pdf == "1") --}}
                                                         <p style="font-family:Arial, Helvetica, sans-serif; color: #74787e;">
